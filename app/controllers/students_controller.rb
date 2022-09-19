efc06398dcb91edeb4b3500f5ddc1f97882144e9
Quinn Sissler <QSissler@gmail.com>
2022-09-19 12:14:15 -0400

    commit tree 59ea4a8faeaf40bdf5f845e0001b5e3226dfa966

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        grades = Student.all.order(:grade).reverse
+        render json: grades
+    end
+
+    def highest_grade
+        highest = Student.all.order(:grade).reverse.first
+        render json: highest
+    end
+
+end
